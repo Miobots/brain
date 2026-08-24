cdd1c54cc31b1a2549a48ac80c8b291460e9470c
AmmarK13 <bscs23059@itu.edu.pk>
2026-08-24 12:47:15 +0500

    feat(sendCommand): implement command sending with ACK handling and timeout

--- src/brain/config.ts
+++ src/brain/config.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 
 const port = Number(process.env.PORT);
 const devToken = process.env.DEV_TOKEN;
+const timeout_ms = Number(process.env.ACK_TIMEOUT)
 
 if (!port) {
     throw new Error("PORT is not set");
@@ -11,7 +12,12 @@ if (!devToken) {
     throw new Error("DEV_TOKEN not present, refusing to start...");
 }
 
+if (!timeout_ms){
+    throw new Error("Timeout not set")
+}
+
 export const config = {
     port,
     devToken,
+    timeout_ms,
 };
